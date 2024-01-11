16a9e0031cd21b24fba0655ed532e5fe190ee9c9
Vincent Mailhot <prog.vincent.mailhot@gmail.com>
2024-01-10 20:38:24 -0500

    Add home view and static file configuration

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,6 +99,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "static/"
+
+STATICFIlES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 
